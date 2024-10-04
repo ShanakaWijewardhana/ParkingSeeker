b816eba8d446e5001bb52ce48d2180c66f95f601
Shanaka Wijewardhana <ngshanaka0530@gmail.com>
2024-10-04 20:10:35 +0530

    codes

--- parking_seeker/src/App.js
+++ parking_seeker/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './home'; // Import your HomePage component with an uppercase H
+import './App.css'; // Global CSS (if needed)
 
+// Main App function
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Render the Home component */}
+      <Home />
     </div>
   );
 }
